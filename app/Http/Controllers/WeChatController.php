079e228182ab73049066e5836ff427c75ac8f172
yunlong <1148117006@qq.com>
2018-10-31 15:30:40 +0800

    a

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -23,7 +23,7 @@ class WeChatController extends Controller
 		];
 		$app = Factory::miniProgram($config);
 		$session = $app->auth->session("023esgYq1F5Wqo0sWYUq1crvYq1esgYT");
-		$decryptedData = $app->encryptor->decryptData($session, "affqG1rRftc56QR56/MRBg==", "dhuykHzDIQAj7Wt2YaL+X5B/xleOQ1WSt8nfXs6rS8x0HXlbESfb2iL1LpUQTudtohMzlOtZ9AHaR4saarkWAyZzQMLmDNkHHI94q46+VBjnaH2UB1+XMb4S5ZIp6rN39XRuO3u+AvUk/FiEPWoNNT7fQZqswvcgKBq+Jyi5xoD9r/6SLicNFpyhxIIfAOnIFmeJ3nc6fM8Nra5acerPaN5iFN/MYd6YGeDr7fZiDwUxZOC5GL2VaQEII3N9KslL64T0xsnOxlNSxzq9jtuq5qBgsGjeZotYF/zx+E3uwJKjW5SP8W1O+wq/olILNXZ5bQ/lDb913g+fhJi7Hd7WNgYNbntdF1zdx/EWanT+MqDovC4IgYdg47Vsxde17C9PSoLqhoha+7rSyYBeRgEmE2dtqaKXMcGZ/wTPDBmyt2ew+e+v3RFSf2zT83+A8TutiCJCp5R80ThjutOx8N1U3SrdI9kqrZ0F/Thxnq3ekBc=");
+		$decryptedData = $app->encryptor->decryptData($session->session_key, "affqG1rRftc56QR56/MRBg==", "dhuykHzDIQAj7Wt2YaL+X5B/xleOQ1WSt8nfXs6rS8x0HXlbESfb2iL1LpUQTudtohMzlOtZ9AHaR4saarkWAyZzQMLmDNkHHI94q46+VBjnaH2UB1+XMb4S5ZIp6rN39XRuO3u+AvUk/FiEPWoNNT7fQZqswvcgKBq+Jyi5xoD9r/6SLicNFpyhxIIfAOnIFmeJ3nc6fM8Nra5acerPaN5iFN/MYd6YGeDr7fZiDwUxZOC5GL2VaQEII3N9KslL64T0xsnOxlNSxzq9jtuq5qBgsGjeZotYF/zx+E3uwJKjW5SP8W1O+wq/olILNXZ5bQ/lDb913g+fhJi7Hd7WNgYNbntdF1zdx/EWanT+MqDovC4IgYdg47Vsxde17C9PSoLqhoha+7rSyYBeRgEmE2dtqaKXMcGZ/wTPDBmyt2ew+e+v3RFSf2zT83+A8TutiCJCp5R80ThjutOx8N1U3SrdI9kqrZ0F/Thxnq3ekBc=");
 		// return $decryptedData;
 		 var_dump($session);exit;
 
